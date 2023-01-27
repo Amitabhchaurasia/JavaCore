eedf9db9440114fe7388aa6fad98566dbbf8baf6
amitabhchaurasia <amitabhchaurasia8@gmail.com>
2023-01-27 19:58:07 +0530

    new more changes

--- InputOutput/byteStream.java
+++ InputOutput/byteStream.java
@@ -7,12 +7,23 @@ public class byteStream {
     void copyByte(){
         FileInputStream in =null;
         FileOutputStream out=null;
+        
         try{
             in=new FileInputStream("demoe.txt");
             out=new FileOutputStream("demoTst.txt");
+            int c;
+        while((c=in.read())!=-1){
+            out.write(c);
+        }
         } catch(Exception e){
             e.printStackTrace();
         }
+        finally {if(in!=null){
+            in.close();
+            if(out!=null){
+                out.close();
+            }
+        }}
     }
     public static void main(String[] args) {
         System.out.println("hell");
