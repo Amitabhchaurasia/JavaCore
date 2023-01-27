e1225e201016617ea4f33b826a6247ebbc4f5f19
amitabhchaurasia <amitabhchaurasia8@gmail.com>
2023-01-27 19:28:06 +0530

    chnage

---
+++ InputOutput/javaIO.java
@@ -0,0 +1,9 @@
+package InputOutput;
+/*
+ * Byte Stream classes : BufferInputStram ,BufferedOutPutStream,
+ */
+public class javaIO {
+    public static void main(String[] args) {
+        
+    }
+}
