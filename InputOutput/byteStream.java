e5992077aea45d140da629a015e575d6bb40f27a
amitabhchaurasia <amitabhchaurasia8@gmail.com>
2023-01-27 19:47:20 +0530

    new changes

---
+++ InputOutput/byteStream.java
@@ -0,0 +1,16 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.*;
+
+public class byteStream {
+    void copyByte(){
+        FileInputStream in =null;
+        FileOutputStream out=null;
+        try{
+            in=new FileInputStream("")
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("hell");
+    }
+}
--- InputOutput/javaIO.java
+++ InputOutput/javaIO.java
@@ -1,9 +1,34 @@
-package InputOutput;
+import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /*
- * Byte Stream classes : BufferInputStram ,BufferedOutPutStream,
+ * Byte Stream classes : BufferInputStram ,
+ * BufferedOutPutStream,DataInputStream ,DataOuptput Stream
+ * FileInputStream,FileOutPutStream
+ * OutPutStream,PrintStream
+ * 
+ * Charater Stream Classes : BufferedReader -> it hadle buufer inputString
+ * BufferWriter -> it hadle outPut.
+ * filereader,FileWrite,InputStreamReader
+ * outPutStreamReader,printWriter its a outtput method 
+ * 
  */
 public class javaIO {
+    void readConsoleInput(){
+        BufferedReader obj =new BufferedReader(new InputStreamReader(System.in));
+        try{
+            System.out.println("Enter the string ");
+            String str=obj.readLine();
+            System.out.println("Write:"+str);
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
-        
+        javaIO obj =new javaIO();
+        obj.readConsoleInput();
     }
 }
